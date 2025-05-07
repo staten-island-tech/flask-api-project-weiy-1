104a90d5e087132fcb9114d094715aaa90d96e3f
weiy-1 <weiyuanyan1126@gmail.com>
2025-05-07 19:01:22 -0400

    filter

--- app.py
+++ app.py
@@ -7,7 +7,42 @@ app = Flask(__name__)
 def index():
     response = requests.get("https://raw.githubusercontent.com/neelpatel05/periodic-table-api/refs/heads/master/data.json")
     data = response.json()
-    return render_template("index.html", elements=data)
+
+    # --- Filtering ---
+    group_block = request.args.get("groupBlock")
+    standard_state = request.args.get("standardState")
+
+    if group_block:
+        data = [el for el in data if el["groupBlock"].lower() == group_block.lower()]
+    if standard_state:
+        data = [el for el in data if el["standardState"].lower() == standard_state.lower()]
+
+    # --- Sorting ---
+    sort_by = request.args.get("sortBy")
+    order = request.args.get("order", "asc")
+
+    if sort_by:
+        reverse = order == "desc"
+        try:
+            data.sort(key=lambda x: float(x.get(sort_by, 0)) if x.get(sort_by) not in [None, ""] else 0, reverse=reverse)
+        except ValueError:
+            data.sort(key=lambda x: x.get(sort_by, "").lower(), reverse=reverse)
+
+    # For filter options in the form
+    group_blocks = sorted(set(el["groupBlock"] for el in data if el["groupBlock"]))
+    standard_states = sorted(set(el["standardState"] for el in data if el["standardState"]))
+
+    return render_template(
+    "index.html",
+    elements=data,
+    group_blocks=group_blocks,
+    standard_states=standard_states,
+    selected_group_block=group_block,
+    selected_standard_state=standard_state,
+    selected_sort_by=sort_by,
+    selected_order=order
+)
+
 
 @app.route("/element/<symbol>")
 def element(symbol):
